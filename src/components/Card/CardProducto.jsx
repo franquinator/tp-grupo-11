b3c1877b4f2c72bdfda90ae668d1bf06f2a1afbe
FacundoNicolasTorres <facujaritor@gmail.com>
2025-10-01 12:02:56 -0300

    data productos y cards de estos mismos en carta

--- src/components/Card/CardProducto.jsx
+++ src/components/Card/CardProducto.jsx
@@ -6,7 +6,7 @@ export default function CardProducto( { producto } ){
             <img src={producto.imagen} className="card-img-top" alt={producto.nombre}/>
             <div className="card-body">
                 <h5 className="card-title">{producto.nombre}</h5>
-                <p className="card-text text-body-secondary">holaa{producto.descripcion}</p>
+                <p className="card-text text-body-secondary">{producto.descripcion}</p>
                 <h5 className="card-title">${producto.precio}</h5>
                 <div className="d-grid gap-2 col-6 mx-auto">
                     <button className="btn btn-dark" type="button">+ Agregar al carrito</button>
--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import productos from '../../data/productos.json'
 import CardProducto from "../components/Card/CardProducto";
 
+
 export default function Carta() {
   return (
+    /*Paso producto del array como parametro a cardProducto y arma la carta con los valores del objeto*/
     <>
-      <CardProducto producto={productos[0]} />
+      <CardProducto producto={productos[0]} /> 
+      <CardProducto producto={productos[1]} />
+      <CardProducto producto={productos[2]} />
+      <CardProducto producto={productos[3]} />
+      <CardProducto producto={productos[4]} />
+      <CardProducto producto={productos[5]} />
+      <CardProducto producto={productos[6]} />
+      <CardProducto producto={productos[7]} />
+      <CardProducto producto={productos[8]} />
+      <CardProducto producto={productos[9]} />
     </>
 );
 }
