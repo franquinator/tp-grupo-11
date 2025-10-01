2602c21302b98ab39aa8052a47c124c0b78abbd8
FacundoNicolasTorres <facujaritor@gmail.com>
2025-10-01 13:22:42 -0300

    CardProducto con imagenes de mismo tamaño y animacion

--- src/components/Card/CardProducto.jsx
+++ src/components/Card/CardProducto.jsx
@@ -2,8 +2,10 @@ import style from './CardProducto.module.css'
 
 export default function CardProducto( { producto } ){
     return(
-        <div className="card" style={{width: "18rem"}}>
-            <img src={producto.imagen} className="card-img-top" alt={producto.nombre}/>
+        <div className={`card ${style.cardProd}`} style={{width: "18rem"}}>
+            <div className={style.divImgCard}>
+            <img src={producto.imagen} className={`card-img-top img-fluid float-end ${style.imgCard}`} alt={producto.nombre}/>
+            </div>
             <div className="card-body">
                 <h5 className="card-title">{producto.nombre}</h5>
                 <p className="card-text text-body-secondary">{producto.descripcion}</p>
