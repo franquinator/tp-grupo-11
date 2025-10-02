23992f4b365fc521c44d7c9be2ac0c5fcab83cb5
Franco <francoiannone2@gmail.com>
2025-10-02 11:23:13 -0300

    carta responsive

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import productos from '../../data/productos.json'
 import CardProducto from "../components/Card/CardProducto";
 import Buscador from "../components/Buscador";
@@ -6,6 +5,7 @@ import Buscador from "../components/Buscador";
 
 export default function Carta() {
   return (
+    
     /*Paso producto del array como parametro a cardProducto y arma la carta con los valores del objeto*/
     <>
       <h2 className="text-center x-auto p-3">Nuestra Carta</h2>
@@ -14,45 +14,8 @@ export default function Carta() {
 
       <Buscador/>
 
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <CardProducto producto={productos[0]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[1]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[2]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[3]} /> 
-          </div>
-        </div>
-        
-        <div className="row">
-          <div className="col">
-            <CardProducto producto={productos[4]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[5]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[6]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[7]} /> 
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <CardProducto producto={productos[8]} /> 
-          </div>
-          <div className="col">
-            <CardProducto producto={productos[9]} /> 
-          </div>
-        </div>
+      <div className="container d-flex p-2 flex-wrap justify-content-evenly">
+          {productos.map(p => <CardProducto producto={p}/>)}
       </div>
     </>
 );
