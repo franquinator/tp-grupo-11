231ba85a6d5f00334ee768c4c0ce6ede0cef03b3
FacundoNicolasTorres <facujaritor@gmail.com>
2025-10-02 23:03:38 -0300

    pagina del carrito sin productos

---
+++ src/components/Card/CardCarrito.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from 'CardCarrito.module.css'
+
+export default function CardCarrito({ producto }) {
+  return (
+    <div>
+        <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+</div>
+    </div>
+  )
+}
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import style from './Carrito.module.css'
+import ButtonLink from "../components/ButtonLink.jsx";
 
 export default function Carrito() {
-  return <></>;
+  return (
+    <>
+      <div id="Carrito sin productos">
+        <div className={`d-flex justify-content-center align-items-center ${style.divLogoCompra}`}>
+          <i className={`bi bi-bag-plus ${style.logoCompra}`}></i>
+        </div>
+        <h2 className="text-center">Tu carrito está vacío</h2>
+        <p className="text-center text-secondary">Parece que aún no has agregado ningún producto a tu carrito. ¡Explora nuestra deliciosa carta!</p>
+        <p className="text-center text-secondary">¡Explora nuestra deliciosa carta!</p>
+        <div className="d-flex justify-content-center align-items-center">
+          <ButtonLink pagina={"/carta"} texto={"Ver Carta"}/>
+        </div>
+      </div>
+    </>
+  );
 }
